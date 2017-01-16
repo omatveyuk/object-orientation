e9a3f9e1f657fd74f6a07c4b8c0a112f657664f2
vagrant <vagrant@vagrant.vm>
2017-01-16 01:43:07 +0000

    Part 2. Create classes

--- assessment.py
+++ assessment.py
@@ -39,3 +39,23 @@ Part 1: Discussion
 
 # Parts 2 through 5:
 # Create your classes and class methods
+class Student(object):
+    """Information about student"""
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+class Question(object):
+    """Representation of a question and answer"""
+    def __init__(self, question, answer):
+        self.question = question
+        self.correct_answer = answer
+
+
+class Exam(object):
+    """Representation questions for an exam"""
+    def __init__(self, name):
+        self.name = name
+        self.questions = []
